b101d1d9601cc6354e982cbb7b24f2218020192d
Sky <sky.pierce@bellevuecollege.edu>
2018-10-10 12:25:37 -0700

    button stuff

--- ListButton/index.js
+++ ListButton/index.js
@@ -1,7 +1,8 @@
 window.onload = () => {
     const userInput = document.getElementById('list-data');
     const setTextButton = document.getElementById('set-text');
-    setTextButton.onclick = () => {
-        userinput.value= 'the first item on my list'
-    }
 }
+
+    setTextButton.onclick = () => {
+        userinput.value= 'the first item on my list';
+    }
