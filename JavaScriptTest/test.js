6a687f139c14e97986841ede0175d114ff4ccd19
Sky Pierce <sky.pierce@bellevuecollege.edu>
2018-10-10 18:23:13 +0000

    edit README

---
+++ JavaScriptTest/test.js
@@ -0,0 +1,11 @@
+var introduction= "I breathe with the light of one thousand electrons, how may I help you?";
+var a= 10;
+var b= 15;
+var addition= "I can add two terms with relative ease, your sum should total as...";
+console.log(introduction);
+console.log(addition)
+console.log('a+b=',a+b);
+
+window.alert(introduction);
+window.alert(addition)
+window.alert('a+b=',a+b);
