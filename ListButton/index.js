24e8617f6426c2137a7371e9746fcb0f7b67f4a9
Sky <sky.pierce@bellevuecollege.edu>
2018-10-17 12:23:19 -0700

    final push

--- ListButton/index.js
+++ ListButton/index.js
@@ -1,12 +1,14 @@
 window.onload = () => {
-    const setTextButton = document.getElementById('set-text');
     const userInput = document.getElementById('list-data');
+    const setTextButton = document.getElementById('set-text');
     const listButton = document.getElementById('add-to-list');
     
  setTextButton.onclick = () => {
-        userInput.value ='List Item';}
+        userInput.value ='List Item';
+    }
 
 listButton.onclick = () => {
     const myList = document.getElementById('my-list');
-    elfCode.appendToList(myList, userInput.value);}
+    elfCode.appendToList(myList, userInput.value);
+}
 } 
