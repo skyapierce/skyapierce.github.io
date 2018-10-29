2056a3823be2d852af2b53ec4e42a787756b94ab
Sky <sky.pierce@bellevuecollege.edu>
2018-10-29 11:27:30 -0700

    classs

---
+++ JavaScriptTest/week7day1.js
@@ -0,0 +1,14 @@
+const num= 3; 
+const numberString = 3;
+const rain = false; 
+
+console.log(num, numberString, rain);
+console.log(typeof num, typeof numberString, typeof rain);
+console.log(num + numberString);
+console.log(typeof num);
+
+if (rain === true) { 
+    console.log('go to the gym');
+} else {
+    console.log('go for a walk');
+}
--- numberstring.js
+++ numberstring.js
@@ -1,6 +1,6 @@
 const num= 3; 
 const numberstring = '3';
 
-console.log(num, numberstring);
-console.log(typeof num, typeof numberstring);
+console.log(num, numberstring, rain);
+console.log(typeof num, typeof numberstring, typeof rain);
 console.log(num = numberstring);
