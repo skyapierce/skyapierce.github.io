0b95cc7d136d7f948a362b7b9b27136889d54259
Sky <sky.pierce@bellevuecollege.edu>
2018-10-31 12:21:58 -0700

    for loop

---
+++ for-loops/index.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    const listButton = document.getElementById('run-for-loop');
+
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+
+        for (let i = 0; i < 7; i++) { 
+            elfCode.appendToList(myList, i);
+        }
+    }
+}                    
+        
--- javascript-test/week7day2.js
+++ javascript-test/week7day2.js
@@ -18,4 +18,16 @@ for (let i = 0; i < 7; i++) {
     console.log(i);
 }
 
+showMessage("===== While Loops =====");
+
+let looper = 10; 
+
+while (looper < 15) { 
+    console.log(looper++);
+}
+
+do { 
+    console.log(looper++);
+} while (looper < 20); 
+
 showMessage("====== Complete ======="); 
