063ea2109ca28bdcf2970d0113fe604e6dbf0c39
Sky <sky.pierce@bellevuecollege.edu>
2018-10-31 11:37:24 -0700

    classwork

--- javascript-test/week7day2.js
+++ javascript-test/week7day2.js
@@ -1,3 +1,21 @@
-const result = 1 +2 * 3;
+var showMessage = (message) => { 
+   console.log("===================================");
+   console.log("===== " + message + " =====");
+   console.log("===================================");
+}
 
-console.log(result)
+showMessage('= Operator Precedence =');
+
+let result = 1 + 2 * 3;
+console.log(result);
+
+result = (1 + 2) * 3;
+console.log(result);
+
+showMessage("====== for loops ======");
+
+for (let i = 0; i < 7; i++) {
+    console.log(i);
+}
+
+showMessage("====== Complete ======="); 
