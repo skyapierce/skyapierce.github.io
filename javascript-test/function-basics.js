11e40dccce46f55ddd7c23f56d4188564b3ceb14
Sky <sky.pierce@bellevuecollege.edu>
2018-11-05 12:16:17 -0800

    parameters

---
+++ javascript-test/function-basics.js
@@ -0,0 +1,23 @@
+function simplefunction() {
+    console.log("function text")
+}
+
+simplefunction();
+
+function functionparameters(value) { 
+    console.log (value)
+}
+
+functionparameters("this function has a parameter"); 
+
+function add(a , b) { 
+    console.log(a+b);
+}
+
+add(2 , 3)
+
+function adder () { 
+    console.log(2+3)
+}
+
+adder();
