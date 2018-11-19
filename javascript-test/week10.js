6c363ae981dd401c36372718bbe0e01a4e966072
Sky <sky.pierce@bellevuecollege.edu>
2018-11-19 12:16:52 -0800

    week 10

--- javascript-test/week10.js
+++ javascript-test/week10.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     const objectLiteralAction = document.getElementById("object-literal-action")
+    const numbersAction = document.getElementById("numbers-action");
 
     objectLiteralAction.onclick = () => {
         const objectLiteralDisplay = document.getElementById("object-literal-display")
@@ -8,11 +9,21 @@ window.onload = () => {
         objectLiteralDisplay.textContent = objectLiteral.getName();  
     }
 
+    numbersAction.onclick = () => { 
+        const numbersDisplay = document.getElementById("numbers-display");
+        numbersDisplay.textContent = numbers;
+        for (let number of numbers) {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(number))
+            numbersDisplay.appendChild(li)
+        }
+    }
 }
+
+const numbers = [2,1,3];
 
 const objectLiteral = {
     objectName: "object literal is my name",
-    
     getName: function () {
         return this.objectName;
     }
